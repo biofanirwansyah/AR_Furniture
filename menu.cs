566b79616d7effa14649021b2f7c5973390df603
Bio Fanirwansyah <43460405+biofanirwansyah@users.noreply.github.com>
2019-03-18 23:24:33 +0700

    Add files via upload

---
+++ menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    public void menu_meja()
+    {
+        Application.LoadLevel("menumeja");
+    }
+
+    public void menu_kursi()
+    {
+        Application.LoadLevel("menukursi");
+    }
+
+    public void menu_kasur()
+    {
+        Application.LoadLevel("menukasur");
+    }
+
+    public void menu_lemari()
+    {
+        Application.LoadLevel("menulemari");
+    }
+
+    public void menu_utama()
+    {
+        Application.LoadLevel("menuutama");
+    }
+}
